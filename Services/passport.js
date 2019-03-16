eaf690e42f82abb08e4b652f6cecccba77e5f2a6
Julian Hoyos <jhoyos135@gmail.com>
2019-03-15 20:05:16 -0400

    finish backend google and passport functionality

---
+++ Services/passport.js
@@ -0,0 +1,55 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+
+    done(null, user.id);
+ 
+ });
+ 
+ passport.deserializeUser((id, done) => {
+ 
+    User.findById(id)
+       .then(user => {
+          done(null, user);
+       });
+ 
+ });
+
+passport.use( new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    proxy: true
+},
+async (accessToken, refreshToken, profile, done) => {
+
+    const existingUser =  await User.findOne({
+
+       googleID: profile.id,
+       displayName: profile.displayName 
+    
+    });
+
+          if(existingUser) {
+             // there's already a record of the user
+             done(null, existingUser);
+
+          } else {
+             // no record of this user, so save it to Mongo
+             const user = await new User({
+
+                googleID: profile.id,
+                displayName: profile.displayName
+
+             }).save();
+
+             done(null, user);
+          }
+       
+}));
--- app.js
+++ app.js
@@ -3,15 +3,25 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./Models/User');
+require('./Services/passport');
 
+mongoose.connect(keys.MongoURI, {useNewUrlParser: true})
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.send('hello')
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 2,
+    keys: [keys.cookieKey]
 })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./Routes/authRoutes')(app);
 
 
 if(process.env.NODE_ENV === 'production') {
